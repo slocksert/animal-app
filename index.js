7d25087146678312f2f0fc1466045ebaa77cdafb
slocksert <rsdlj01@gmail.com>
2022-11-15 15:52:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+async function carregarAnimais(){
+    const response = await axios.get('http://localhost:8000/animais')
+
+    const animais = response.data
+
+    const lista = document.getElementById('lista-animais')
+
+    lista.innerHTML = ''
+
+    animais.forEach(animal => {
+        const item = document.createElement('li')
+
+        const linha = `${animal.nome} - Idade: ${animal.idade} - Sexo: ${animal.sexo} - Cor: ${animal.cor}`
+        
+        item.innerText = linha
+        
+        lista.appendChild(item)
+    })
+}
+
+function manipularFormulario(){
+    const form_animal = document.getElementById('form-animal')
+    const input_nome = document.getElementById('nome')
+    const input_idade = document.getElementById('idade')
+    const input_sexo = document.getElementById('sexo')
+    const input_cor = document.getElementById('cor')
+
+    form_animal.onsubmit = async (event) => {
+        event.preventDefault()
+        const nome_animal = input_nome.value
+        const idade_animal = input_idade.value
+        const sexo_animal = input_sexo.value
+        const cor_animal = input_cor.value
+
+        await axios.post('http://localhost:8000/animais', {
+            nome: nome_animal,
+            idade: idade_animal,
+            sexo: sexo_animal,
+            cor: cor_animal
+        })
+        
+        carregarAnimais()
+        alert(`${nome_animal} cadastrado`)
+    }
+}
+
+function App(){
+    console.log('App iniciado')
+    carregarAnimais()
+    manipularFormulario()
+}
+
+App()
---
+++ server.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from uuid import uuid4
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ['http://localhost:5500']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+class Animal(BaseModel):
+    id: str | None = None
+    nome: str
+    idade: int
+    sexo: str
+    cor: str
+
+banco: List[Animal] = []
+
+@app.get('/animais')
+async def visualizar():
+    return banco
+
+@app.post('/animais')
+async def adicionar(animal: Animal):
+    animal.id = str(uuid4())
+    banco.append(animal)
+    return {'dados': f'Nome:{animal.nome} Idade:{animal.idade} Sexo: {animal.sexo} Cor: {animal.cor}'}
+
+@app.get('/animais/{animal_id}')
+async def visualizar_id(animal_id: str):
+    for animal in banco:
+        if animal.id == animal_id:
+            return animal
+    return { 'erro': 'Animal não localizado'}
+
+@app.delete('/animais/{animal_id}')
+async def delete_id(animal_id: str):
+    for animal in banco:
+        if animal.id == animal_id:
+            banco.remove(animal)
+            return { "mensagem": f"{animal_id} foi removido!"}
